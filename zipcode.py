f716e1225003c9c72e56a87c96b666f80b2d08f8
Aviral Singh <79758057+Avirall@users.noreply.github.com>
2023-10-23 22:08:36 +0530

    Add files via upload

--- zipcode.py
+++ zipcode.py
@@ -1,26 +1,41 @@
 import csv
 import matplotlib.pyplot as plt
-zipcodes=[]
-district_count={}
-district_name={}
-with open('Dataset/Maharashtra.csv',newline='',encoding='ISO-8859-1') as csvfile:
-    dataset= csv.DictReader(csvfile)
-    for row in dataset:
-        row['DATE_OF_REGISTRATION']=str(row['DATE_OF_REGISTRATION'])
-        if row['DATE_OF_REGISTRATION'][-2:]=='15' and row['Registered_Office_Address'][-6:]!='000000':
-            val=row['Registered_Office_Address'][-6:]
-            zipcodes.append(val)
-            
-print(len(zipcodes))
-with open('Dataset/pincode.csv',newline='',encoding='ISO-8859-1') as csvfile:
-    dataset= csv.DictReader(csvfile)
-    
-    for row in dataset:
-        district_name[row['Pin Code']]=row['District']
-        district_count[row['District']]=0
-for i in zipcodes:
-    if i in district_name.keys():
-        district_count[district_name[i]]+=1
 
-plt.bar(district_count.keys(),district_count.values())
-plt.show()
+# Function to process data and create the bar chart
+def process_and_plot_district_count(data_csv_path, pincode_csv_path):
+    zipcodes = []
+    district_count = {}
+    district_name = {}
+
+    with open(data_csv_path, newline='', encoding='ISO-8859-1') as csvfile:
+        dataset = csv.DictReader(csvfile)
+
+        for row in dataset:
+            row['DATE_OF_REGISTRATION'] = str(row['DATE_OF_REGISTRATION'])
+            if row['DATE_OF_REGISTRATION'][-2:] == '15' and row['Registered_Office_Address'][-6:] != '000000':
+                val = row['Registered_Office_Address'][-6:]
+                zipcodes.append(val)
+
+    with open(pincode_csv_path, newline='', encoding='ISO-8859-1') as csvfile:
+        dataset = csv.DictReader(csvfile)
+
+        for row in dataset:
+            district_name[row['Pin Code']] = row['District']
+            district_count[row['District']] = 0
+
+    for i in zipcodes:
+        if i in district_name.keys():
+            district_count[district_name[i]] += 1
+
+    return district_count
+
+# Function to create and display the bar chart
+def plot_district_count(district_count):
+    plt.bar(district_count.keys(), district_count.values())
+    plt.show()
+
+# Usage
+data_csv_path = 'Dataset/Maharashtra.csv'
+pincode_csv_path = 'Dataset/pincode.csv'
+district_count = process_and_plot_district_count(data_csv_path, pincode_csv_path)
+plot_district_count(district_count)
